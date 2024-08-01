b78304ef2a1a3940ea345a71a2ef6520b62fe35c
Larissa Goularte <58703854+larissagoularte@users.noreply.github.com>
2024-08-01 23:10:35 +0100

    put route fix

--- routes/listingRoutes.js
+++ routes/listingRoutes.js
@@ -7,7 +7,7 @@ const { upload, uploadToR2 } = require('../middlewares/upload');
 router.post('/add', isAuthenticated, upload.array('media', 20), listingController.addListing);
 router.get('/', isAuthenticated, listingController.fetchListings);
 router.get('/:id', listingController.fetchListingById);
-router.put('/:id', isAuthenticated, upload, listingController.updateListing);
+router.put('/:id', isAuthenticated, upload.array('media', 20), listingController.updateListing);
 router.delete('/:id', isAuthenticated, listingController.removeListing);
 router.delete('/:id/images/:image', isAuthenticated, listingController.removeImage);
 
